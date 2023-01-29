21192179d30af2aaa4c645147f1341bfe5be0f69
lapatric <42653152+lapatric@users.noreply.github.com>
2023-01-30 00:21:09 +0100

    add basic hardhat config

--- v2-core-fork/hardhat.config.js
+++ v2-core-fork/hardhat.config.js
@@ -1,4 +1,60 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.5.16",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+      mining: {
+        auto: true, // set to FALSE when testing frontend for a more realistic experience!
+        interval: [3000, 6000] // only relevant when {auto: FALSE}
+      }
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: 6,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+        default: 0, // deployer by default
+    },
+  },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+        rinkeby: ETHERSCAN_API_KEY,
+        goerli: ETHERSCAN_API_KEY,
+    },
+  },
+  gasReporter: { // print gas output when running tests
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    // coinmarketcap: "COINMARKETCAP_API_KEY",
+    token: "MATIC",
+  },
+  mocha: {
+    timeout: 300000, // timeout on test afer 300ms
+  }
 };
