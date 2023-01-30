d798d4813eb487cb215fdc732582a63a2210485a
lapatric <42653152+lapatric@users.noreply.github.com>
2023-01-30 11:12:20 +0100

    implement basic ERC20 token

---
+++ v2-core-fork/contracts/TokenERC20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenERC20 is ERC20 {
+    
+    constructor(uint256 initialSupply, string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+
+}
--- v2-core-fork/hardhat.config.js
+++ v2-core-fork/hardhat.config.js
@@ -12,7 +12,16 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
-  solidity: "0.5.16",
+  solidity: {
+    compilers: [{ version: "0.5.16"}, { version: "0.8.0"}, { version: "0.8.9"} ],
+    // version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: false,
+        runs: 99999,
+      },
+    },
+  },
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
