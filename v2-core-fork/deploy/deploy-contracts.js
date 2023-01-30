f263cdc745d2b80efa527801f8bbf0f72f54a2db
lapatric <42653152+lapatric@users.noreply.github.com>
2023-01-30 11:13:45 +0100

    deploy two ERC20 tokens

--- v2-core-fork/deploy/deploy-contracts.js
+++ v2-core-fork/deploy/deploy-contracts.js
@@ -1,12 +1,30 @@
-const { network } = require("hardhat")
+const { ethers, network } = require("hardhat")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments; // https://www.npmjs.com/package/hardhat-deploy
     const { deployer } = await getNamedAccounts();
 
+    console.log("Deploying smart contracts...");
+
     await deploy("UniswapV2Factory", {
         from: deployer,
         args: [deployer], // constructor arguments
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    await deploy("TokenA", {
+        from: deployer,
+        contract: "TokenERC20",
+        args: [ethers.utils.parseEther("1000"), "TokenA", "TKA"], // constructor arguments
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    await deploy("TokenB", {
+        from: deployer,
+        contract: "TokenERC20",
+        args: [ethers.utils.parseEther("10000"), "TokenB", "TKB"], // constructor arguments
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
