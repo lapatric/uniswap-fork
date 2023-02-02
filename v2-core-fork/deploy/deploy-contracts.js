b33289acd743b445788fd0b08a9e72b6ca5a0fb8
lapatric <42653152+lapatric@users.noreply.github.com>
2023-02-02 17:28:14 +0100

    add deploy code for LP migration

--- v2-core-fork/deploy/deploy-contracts.js
+++ v2-core-fork/deploy/deploy-contracts.js
@@ -1,12 +1,11 @@
-const { ethers, network } = require("hardhat")
-
+const { ethers, network } = require("hardhat");
+const { IUniswapV2Factory } = require('@uniswap/v2-core');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments; // https://www.npmjs.com/package/hardhat-deploy
     const { deployer } = await getNamedAccounts();
 
-    console.log("Deploying smart contracts from contracts/core...");
-
+    console.log("------------------------------- Deploy smart contracts from contracts/core -------------------------------");
     const factory = await deploy("UniswapV2Factory", {
         from: deployer,
         args: [deployer], // constructor arguments
@@ -14,7 +13,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         waitConfirmations: network.config.blockConfirmations || 1,
     });
 
-    await deploy("TokenA", {
+    const tokenA = await deploy("TokenA", {
         from: deployer,
         contract: "TokenERC20",
         args: [ethers.utils.parseEther("1000"), "TokenA", "TKA"], // constructor arguments
@@ -22,7 +21,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         waitConfirmations: network.config.blockConfirmations || 1,
     });
 
-    await deploy("TokenB", {
+    const tokenB = await deploy("TokenB", {
         from: deployer,
         contract: "TokenERC20",
         args: [ethers.utils.parseEther("10000"), "TokenB", "TKB"], // constructor arguments
@@ -30,23 +29,42 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         waitConfirmations: network.config.blockConfirmations || 1,
     });
 
-    log("-------------------------------------------------------------------")
-    
-    console.log("Deploying smart contracts from contracts/periphery...");
+    console.log("------------------------------- Create pair through deployed factory contract -------------------------------");
+    const pairAddress = await factory.createPair(tokenA.address, tokenB.address);
+    console.log(`Pair deployed to ${pairAddress}.`);
 
+    
+    console.log("-------------------------------Deploy smart contracts from contracts/periphery-------------------------------");
     const weth = await deploy("WETH", {
         from: deployer,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
     
-    await deploy("UniswapV2Router02", {
+    const router = await deploy("UniswapV2Router02", {
         from: deployer,
         args: [factory.address, weth.address], // constructor arguments
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
 
+    log("------------------------------- Deploy smart contracts from contracts/migration -------------------------------");
+    const bonusToken = await deploy("BonusToken", {
+        from: deployer,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    const uniswapRouterAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+    const uniswapPairAddress = IUniswapV2Factory(uniswapRouterAddress).getPair(tokenA.address, tokenB.address);
+    const liquidityMigrator = await deploy("LiquidityMigration", {
+        from: deployer,
+        args: [uniswapRouterAddress, uniswapPairAddress, router.address, pairAddress, bonusToken.address], // constructor arguments
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    await bonusToken.setMigrator(liquidityMigrator.address);
 }
 
 module.exports.tags = ["all", "factory"];
